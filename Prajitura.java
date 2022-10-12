29243ab9086681b48c3b22052ae4e4b27193f18e
Alexa Ioana <114979323+ioana26a@users.noreply.github.com>
2022-10-12 21:42:09 +0300

    Update Prajitura.java

--- Prajitura.java
+++ Prajitura.java
@@ -1,15 +1,11 @@
 import java.util.Scanner;
-
-public class Prajitura {                        //Clasa este folosita pentru a crea meniul cofetariei
+public class Prajitura {
         private String nume;
         private int pret;
-        public Prajitura(String nume,int pret){
-                this.nume=nume;
-                this.pret=pret;
+        public Prajitura(){
+
         }
-        public String getNume() {
-                return nume;
-        }
+
         public void setNume(String nume) {
                 this.nume = nume;
         }
@@ -18,27 +14,16 @@ public class Prajitura {                        //Clasa este folosita pentru a c
                 this.pret = pret;
         }
 
+        public String getNume() {
+                return nume;
+        }
+
         public String toString(){
                 return "Prajitura " + nume + " costa " + pret + " lei.";
         }
+        Scanner sc=new Scanner(System.in);
 
-        public void actualizarePrajitura(){           //Actualizeaza numele si/sau pretul unei prajituri din meniul cofetariei
-                int opt=-1;
-                Scanner sc=new Scanner(System.in);
-                while(opt!=0){
-                        System.out.println("1.Actualizare nume prajitura");
-                        System.out.println("2.Actualizare pret prajitura");
-                        System.out.println("0.Iesire");
-                        System.out.print("Alegeti o optiune ");
-                        opt=sc.nextInt();
-                        if(opt==1){
-                                System.out.print("Introduceti nume ");
-                                nume=sc.next();
-                        } else if (opt==2) {
-                                System.out.print("Introduceti pret ");
-                                pret= sc.nextInt();
-                        }
-                        else return;
-                }
-        }
+
+
+
 }

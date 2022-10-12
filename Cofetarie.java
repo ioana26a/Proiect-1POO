5e6123b4db9ae10cb17ffc2c47325c17b20f684e
Alexa Ioana <114979323+ioana26a@users.noreply.github.com>
2022-10-12 21:40:52 +0300

    Update Cofetarie.java

--- Cofetarie.java
+++ Cofetarie.java
@@ -5,7 +5,6 @@ public class Cofetarie {
         private String nume;
         private int oraInchidere;
         private int oraDeschidere;
-
         ArrayList<Prajitura> meniuCofetarie = new ArrayList<>();
 
         Scanner sc=new Scanner(System.in);
@@ -25,6 +24,27 @@ public class Cofetarie {
                 return (n>=oraDeschidere&&n<=oraInchidere);
         } //Verifica daca cofetaria este deshisa/inchisa
 
+        public void actualizareCofetarie(){               //Actualizeaza numele si/sau programul cofetariei
+                int opt=-1;
+                while(opt!=0){
+                        System.out.println("\n1.Actualizare nume ");
+                        System.out.println("2.Actualizare program");
+                        System.out.println("0.Iesire\n");
+                        System.out.print("Alegeti o optiune ");
+                        opt=sc.nextInt();
+                        if(opt==1){
+                                System.out.print("Scrieti nume ");
+                                nume= sc.next();
+                        } else if (opt==2) {
+                                System.out.print("Introduceti ora de deschidere ");
+                                oraDeschidere= sc.nextInt();
+                                System.out.print("Introduceti ora de inchidere ");
+                                oraInchidere= sc.nextInt();
+
+                        }
+                        else return;
+                }
+        }
         public void initializareMeniu(){                //creeaza un meniu initial dupa ce a fost creat obiectul cofetarie
                 Scanner sc=new Scanner(System.in);
                 System.out.print("Introduceti numarul de prajituri ");
@@ -38,46 +58,38 @@ public class Cofetarie {
                 for(Prajitura p:meniuCofetarie)
                         System.out.println(p.toString());
         }
-        public void actualizareCofetarie(){               //Actualizeaza numele si/sau programul cofetariei
+        public void adaugarePrajitura(){
+                Prajitura p=new Prajitura();
+                System.out.print("Dati nume:");
+                p.setNume(sc.next());
+                System.out.print("Dati pret:");
+                p.setPret(sc.nextInt());
+                meniuCofetarie.add(p);
+        }
+        public void actualizarePrajitura(String numeCautat){
+                Prajitura pr=null;
+                for (Prajitura p : meniuCofetarie) {
+                        if (p.getNume().equals(numeCautat)){
+                                pr=p;
+                        }
+                }
                 int opt=-1;
+                Scanner sc=new Scanner(System.in);
                 while(opt!=0){
-                        System.out.println("\n1.Actualizare nume ");
-                        System.out.println("2.Actualizare program");
-                        System.out.println("0.Iesire\n");
+                        System.out.println("1.Actualizare nume prajitura");
+                        System.out.println("2.Actualizare pret prajitura");
+                        System.out.println("0.Iesire");
                         System.out.print("Alegeti o optiune ");
                         opt=sc.nextInt();
                         if(opt==1){
-                                System.out.print("Scrieti nume ");
-                                String numeNou= sc.next();
-                                nume=numeNou;
+                                System.out.print("Introduceti nume ");
+                                pr.setNume(sc.next());
                         } else if (opt==2) {
-                                System.out.print("Introduceti ora de deschidere ");
-                                int oraDes= sc.nextInt();
-                                System.out.print("Introduceti ora de inchidere ");
-                                int oraInch= sc.nextInt();
-                                oraDeschidere=oraDes;
-                                oraInchidere=oraInch;
-
+                                System.out.print("Introduceti pret ");
+                                pr.setPret(sc.nextInt());
                         }
                         else return;
                 }
         }
-        public void adaugarePrajitura(){                        //Creeaza un obiect prajitura
-                Prajitura p=new Prajitura();
-                meniuCofetarie.add(p);
-                System.out.print("Introduceti nume prajitura ");
-                p.setNume(sc.next());
-                System.out.print("Introduceti pret prajitura ");
-                p.setPret(sc.nextInt());
-
-        }
-        public void cautarePrajitura(String numeCautat) {        //Cauta o prajitura dupa nume dupa care o actualizeaza
-                for (Prajitura p : meniuCofetarie) {
-                        if (p.getNume().equals(numeCautat))
-                                p.actualizarePrajitura();
-                        else return;
-                }
-        }
-
 
 }
